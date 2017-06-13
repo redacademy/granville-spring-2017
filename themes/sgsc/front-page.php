8214f2d299bc41be2ac40cb38f065874429b9d7d
Brian Lau <lauwangtatbrian@gmail.com>
2017-06-13 14:50:58 -0700

    add feautre-gallery style, and add link to the actualy page

--- themes/sgsc/front-page.php
+++ themes/sgsc/front-page.php
@@ -10,51 +10,48 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		<?php if ( have_posts() ) : ?>
-
 			<?php if ( is_home() && ! is_front_page() ) : ?>
 				<header>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 				</header>
 			<?php endif; ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <header class="entry-header">
-            <section class="learn-about-story">
-            <?php if ( has_post_thumbnail() ) : ?>
-              <?php the_post_thumbnail( 'large' ); ?>
-            <?php endif; ?>
-              <div class="front-learn">
-                <h1>Learn About Our Story</h1>
-                <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'about' )) ) ?>"><p>More</p></a>
-              </div>
-              <div class="donate-btn">
-                <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'donate' )) ) ?>"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
-              </div>
-            </section>
-          </header><!-- .entry-header -->
-        </article><!-- #post-## -->
-          <div class="front-learn">
-			<?php endwhile; ?>
 
-      <section class="programs-event-blog">
-        <div class="front-program">
-          <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'programs' )) ) ?>">
-            <h1>Discover Our Programs</h1></a>
-        </div>
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="entry-header">
+          <section class="learn-about-story">
+          <?php if ( has_post_thumbnail() ) : ?>
+            <?php the_post_thumbnail( 'large' ); ?>
+          <?php endif; ?>
+            <div class="front-learn">
+              <h1>Learn About Our Story</h1>
+              <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'about' )) ) ?>"><p>More</p></a>
+            </div>
+            <div class="donate-btn">
+              <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'donate' )) ) ?>"><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+            </div>
+          </section>
+        </header><!-- .entry-header -->
+      </article><!-- #post-## -->
 
-        <div class="front-events">
-          <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'event' )) ) ?>">
-            <h1>Upcomming events</h1></a>
-        </div>
+      <div class="front-program">
+        <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'programs' )) ) ?>">
+          <h1>Discover Our Programs</h1></a>
+      </div>
 
-        <div class="front-blog">
-          <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'blog' )) ) ?>">
-            <h1>Lastest blog posts</h1></a>
-        </div>
-      </section>
+      <div class="front-events">
+        <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'event' )) ) ?>">
+          <h1>Upcomming events</h1></a>
+      </div>
+
+      <div class="front-blog">
+        <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'blog' )) ) ?>">
+          <h1>Lastest blog posts</h1></a>
+      </div>
 
       <section class="feature-gallery">
-        <h1>"Grandma", Mae Irving's 105th birthday</h1>
+        <div class="front-gallery">
+          <h1>"Grandma", Mae Irving's 105th birthday</h1>
+          <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'gallery' )) ) ?>"><p>More</p></a>
       </section>
 
       <section class="involved-join">
@@ -67,10 +64,10 @@ get_header(); ?>
         </div>
       </section>
         
-        <div class="front-join">
-          <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'join us' )) ) ?>">
-            <h1>Join Us or Volunteer</h1></a>
-        </div>
+      <div class="front-join">
+        <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'join us' )) ) ?>">
+          <h1>Join Us or Volunteer</h1></a>
+      </div>
 
 			<?php the_posts_navigation(); ?>
 
