d80dc0337917945b49850df0535fd0668ffc2761
Brian Lau <lauwangtatbrian@gmail.com>
2017-06-12 22:04:38 -0700

    added sections and divs to each section & static h1 title

--- themes/sgsc/front-page.php
+++ themes/sgsc/front-page.php
@@ -9,7 +9,6 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-    <h1>TEST</h1>
 		<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
@@ -18,13 +17,37 @@ get_header(); ?>
 				</header>
 			<?php endif; ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<section class="learn-about-story">
+        <h1>Learn sbout our story</h1>
+      </section>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
+      <section class="programs-event-blog">
+        <div class="front-program">
+          <h1>Discover our programs</h1>
+        </div>
 
-			<?php endwhile; ?>
+        <div class="front-events">
+          <h1>Upcomming events</h1>
+        </div>
 
+        <div class="front-blog">
+          <h1>Lastest blog posts</h1>
+        </div>
+      </section>
+
+      <section class="feature-gallery">
+        <h1>"Grandma", Mae Irving's 105th birthday</h1>
+      </section>
+
+      <section class="involved-join">
+        <div class="front-involved">
+          <h1>Get involved</h1>
+        </div>
+
+        <div class="front-join">
+          <h1>Join Us or Volunteer</h1>
+        </div>
+      </section>
 			<?php the_posts_navigation(); ?>
 
 		<?php else : ?>
@@ -32,7 +55,6 @@ get_header(); ?>
 			<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
 		<?php endif; ?>
-<h1>END TEST</h1>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
