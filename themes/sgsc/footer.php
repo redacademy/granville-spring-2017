df28d44430c73be5bea6b8f59c7643a9a95ff5f5
heyliddle <eric.liddle@gmail.com>
2017-06-13 11:26:09 -0700

    styling footer

--- themes/sgsc/footer.php
+++ themes/sgsc/footer.php
@@ -11,17 +11,16 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<ul>
-						<li>E-News Letter</li>
-						<li>Join Us</li>
-						<li>Donate</li>
-						<li>Contact Us</li>
-						<li>Advertise With Us</li>
+					<span class="footer-links">
+						<p>E-Newsletter</p>
+						<p>Donate</p>
+						<p>Contact Us</p>
+						<p>Advertise With Us</p>
+						<p>Join Us</p>
 					</ul>
-					<p>2017 South Granville Seniors Centre is  Registered Canadian Charity</p>
+					<p>&copy; 2017 South Granville Seniors Centre is  Registered Canadian Charity</p>
 					<i class="fa fa-facebook" aria-hidden="true"></i>
 					<i class="fa fa-twitter" aria-hidden="true"></i>
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly worked on by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
