1262f9fee8c272cd70c491b4b9db615017327e36
heyliddle <eric.liddle@gmail.com>
2017-06-13 05:04:00 -0700

    added footer content and links fontawesome in header

--- themes/sgsc/footer.php
+++ themes/sgsc/footer.php
@@ -11,6 +11,16 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
+					<ul>
+						<li>E-News Letter</li>
+						<li>Join Us</li>
+						<li>Donate</li>
+						<li>Contact Us</li>
+						<li>Advertise With Us</li>
+					</ul>
+					<p>2017 South Granville Seniors Centre is  Registered Canadian Charity</p>
+					<i class="fa fa-facebook" aria-hidden="true"></i>
+					<i class="fa fa-twitter" aria-hidden="true"></i>
 					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly worked on by %s' ), 'WordPress' ); ?></a>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
