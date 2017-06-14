27abae883aaf6b67942e6059889c1d3acd56a9c3
Brian Lau <lauwangtatbrian@gmail.com>
2017-06-14 11:10:53 -0700

    add front-page php class

--- themes/sgsc/front-page.php
+++ themes/sgsc/front-page.php
@@ -35,10 +35,14 @@ get_header(); ?>
 
 
       <section class="program-event-post">
+
         <div class="front-program">
           <a href="<?php echo esc_url( get_permalink(get_page_by_title( 'programs' )) ) ?>">
             <h1>Discover Our Programs</h1></a>
-          <a class="m-button" href="<?php echo esc_url( get_permalink(get_page_by_title( 'about' )) ) ?>"><p>More</p></a>
+          <div class="detail-program">
+            <p>We have a multitude of programs to keep you healthy, active and connected.</p>
+            <a class="m-button" href="<?php echo esc_url( get_permalink(get_page_by_title( 'about' )) ) ?>"><p>More</p></a>
+          </div>
         </div>
 
         <div>
