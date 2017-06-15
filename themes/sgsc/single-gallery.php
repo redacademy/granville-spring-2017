041e5019be6d1f083f1142135b6af08686f43a68
Brian Lau <lauwangtatbrian@gmail.com>
2017-06-15 15:59:28 -0700

    add single-galler.sccs and import to style.scss

--- themes/sgsc/single-gallery.php
+++ themes/sgsc/single-gallery.php
@@ -12,7 +12,33 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="entry-header">
+				<?php if ( has_post_thumbnail() ) : ?>
+					<?php the_post_thumbnail( 'large' ); ?>
+				<?php endif; ?>
+
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+				<div class="entry-meta">
+					<?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?> / <?php red_starter_posted_by(); ?>
+				</div><!-- .entry-meta -->
+			</header><!-- .entry-header -->
+
+			<div class="entry-content">
+				<?php the_content(); ?>
+				<?php
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+						'after'  => '</div>',
+					) );
+				?>
+			</div><!-- .entry-content -->
+
+			<footer class="entry-footer">
+				<?php red_starter_entry_footer(); ?>
+			</footer><!-- .entry-footer -->
+		</article><!-- #post-## -->
 
 			<?php the_post_navigation(); ?>
 
