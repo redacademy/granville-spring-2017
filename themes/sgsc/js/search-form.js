3ecacc94bbf9ee86da70a190e80cd083c30e1e95
heyliddle <eric.liddle@gmail.com>
2017-06-15 12:48:22 -0700

    toggle for search, border added

--- themes/sgsc/js/search-form.js
+++ themes/sgsc/js/search-form.js
@@ -2,13 +2,15 @@
 
   $(document).ready(function() {      
     $('.icon-search').click(function(){
-        $('.search-field').animate({ width: 'toggle' }, 350);
+        $('.search-field').animate({ width: 'toggle' });
+        $('.icon-search').hide();
         $('.search-field').focus();
         return false;
     });
     $(document).click( function(event){
       if( !$('.search-field').is(event.target)){
-      $('.main-navigation .search-field').hide('fast');
+      $('.search-field').hide('slow');
+      $('.icon-search').show();
       }
     });
   });
