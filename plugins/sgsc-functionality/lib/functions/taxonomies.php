a77ab448b841a65040898672a03af10b8240bf98
Brian Lau <lauwangtatbrian@gmail.com>
2017-06-15 11:05:50 -0700

    add register featured gallery taxonomy

--- plugins/sgsc-functionality/lib/functions/taxonomies.php
+++ plugins/sgsc-functionality/lib/functions/taxonomies.php
@@ -6,3 +6,42 @@
  */
 
 // Add your custom taxonomies here...
+
+// Register Featured Gallery Taxonomy
+function featured_gallery_taxonomy() {
+
+	$labels = array(
+		'name'                       => 'featured galleries',
+		'singular_name'              => 'featured gallery',
+		'menu_name'                  => 'featured gallery',
+		'all_items'                  => 'All featured galleries',
+		'parent_item'                => 'Parent featured gallery',
+		'parent_item_colon'          => 'Parent featured gallery:',
+		'new_item_name'              => 'New featured gallery',
+		'add_new_item'               => 'Add New featured gallery',
+		'edit_item'                  => 'Edit featured gallery',
+		'update_item'                => 'Update featured gallery',
+		'view_item'                  => 'View featured gallery',
+		'separate_items_with_commas' => 'Separate featured galleries with commas',
+		'add_or_remove_items'        => 'Add or remove featured galleries',
+		'choose_from_most_used'      => 'Choose from the most used',
+		'popular_items'              => 'Popular featured galleries',
+		'search_items'               => 'Search featured galleries',
+		'not_found'                  => 'Not Found',
+		'no_terms'                   => 'No featured galleries',
+		'items_list'                 => 'featured galleries list',
+		'items_list_navigation'      => 'featured galleries list navigation',
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'featured gallery', array( 'gallery' ), $args );
+
+}
+add_action( 'init', 'featured_gallery_taxonomy', 0 );
