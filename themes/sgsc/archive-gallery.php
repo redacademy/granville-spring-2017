4b6cc1af9fda4aa0898d867953498b2152b68f94
Brian Lau <lauwangtatbrian@gmail.com>
2017-06-16 10:46:13 -0700

    add img and style to proper size

--- themes/sgsc/archive-gallery.php
+++ themes/sgsc/archive-gallery.php
@@ -27,16 +27,20 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<header class="entry-header">
-					<?php if ( has_post_thumbnail() ) : ?>
-						<?php the_post_thumbnail( 'full' ); ?>
-					<?php endif; ?>
+					 <div class="image-position">		 
+						<?php if ( has_post_thumbnail() ) : ?>
+							<?php the_post_thumbnail( 'full' ); ?>
+						<?php endif; ?>
+					 </div>
 
-					<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+					<a class="entry-title" href="<?php echo esc_url( get_permalink() ) ?>">
+						<?php the_title( sprintf( '<h2 >', esc_url( get_permalink() ) ), '</h2>' ); ?>
+					</a>
 
-					<?php if ( 'post' === get_post_type() ) : ?>
-					<div class="entry-meta">
-						<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
-					</div><!-- .entry-meta -->
+						<?php if ( 'post' === get_post_type() ) : ?>
+						<div class="entry-meta">
+							<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+						</div><!-- .entry-meta -->
 					<?php endif; ?>
 				</header><!-- .entry-header -->
 
