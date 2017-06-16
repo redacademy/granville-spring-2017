6f1464c9faaaaf6c7e7fe8782e465b066280c011
Brian Lau <lauwangtatbrian@gmail.com>
2017-06-16 09:47:24 -0700

    add donate bth mixin, finish basic stlye and php set-up for archive-gallery

--- themes/sgsc/archive-gallery.php
+++ themes/sgsc/archive-gallery.php
@@ -9,22 +9,38 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			<section class="lastest-gallery-section">
+				
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
+				<h1 class="page-title">Lastest Gallery Albums</h1>
+				  <div class="donate-btn">
+						<a href="<?php echo esc_url( get_permalink(get_page_by_title( 'donate' )) ) ?>">
+							<i class="fa fa-heart-o" aria-hidden="true">
+								<p>Donate</p>
+							</i>
+							</a>
+  			 </div>
 			</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php if ( has_post_thumbnail() ) : ?>
+						<?php the_post_thumbnail( 'full' ); ?>
+					<?php endif; ?>
 
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
+					<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+					<?php if ( 'post' === get_post_type() ) : ?>
+					<div class="entry-meta">
+						<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+					</div><!-- .entry-meta -->
+					<?php endif; ?>
+				</header><!-- .entry-header -->
+
+			</article><!-- #post-## -->
 
 			<?php endwhile; ?>
 
@@ -36,6 +52,7 @@ get_header(); ?>
 
 		<?php endif; ?>
 
+			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
