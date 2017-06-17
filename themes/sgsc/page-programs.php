cbe325d1b5a1c0d7d7748cdf66bc99748be022b5
Kat Solberg <katsolberg@gmail.com>
2017-06-16 17:11:05 -0700

    style flex boxes on desktop program page

--- themes/sgsc/page-programs.php
+++ themes/sgsc/page-programs.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area container">
 		<main id="main" class="site-main" role="main">
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
