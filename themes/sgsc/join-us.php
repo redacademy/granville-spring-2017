a44d3a444fc27bb65b388bc7e1311b1457cdd0de
heyliddle <eric.liddle@gmail.com>
2017-06-17 21:43:33 -0700

    touched up join-us.php and scss

--- themes/sgsc/join-us.php
+++ themes/sgsc/join-us.php
@@ -7,7 +7,6 @@ get_the_title();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-<!--<?php echo CFS()->get(); ?>-->
 
   <?php if ( has_post_thumbnail() ) {
         the_post_thumbnail();
@@ -17,24 +16,21 @@ get_the_title();
 			<!--<?php while ( have_posts() ) : the_post(); ?>-->
 
 					<?php get_template_part( 'template-parts/content', 'page' ); ?>
- 
-				<div class="join-bg-image">
-				</div>
 
-				<div class="join-us-section">
-					<div class="new-member rectangle">
-						<h2><?php echo CFS()->get( 'new_member' );	?></h2>
-						<p><?php echo CFS()->get( 'new_member_text' );	?></p>
+					<div class="join-us-section">
+						<div class="new-member rectangle">
+							<h2><?php echo CFS()->get( 'new_member' );	?></h2>
+							<p><?php echo CFS()->get( 'new_member_text' );	?></p>
+						</div>
+						<div class="membership-renewal rectangle">
+							<h2><?php echo CFS()->get( 'membership_renewal' );	?></h2>
+							<p><?php echo CFS()->get( 'membership_renewal_text' );	?></p>
+						</div>	
+						<div class="volunteer rectangle">
+							<h2><?php echo CFS()->get( 'volunteer_opportunities' );	?></h2>
+							<p><?php echo CFS()->get( 'volunteer_opportunities_text' );	?></p>
+						</div>
 					</div>
-					<div class="membership-renewal rectangle">
-						<h2><?php echo CFS()->get( 'membership_renewal' );	?></h2>
-						<p><?php echo CFS()->get( 'membership_renewal_text' );	?></p>
-					</div>	
-					<div class="volunteer rectangle">
-						<h2><?php echo CFS()->get( 'volunteer_opportunities' );	?></h2>
-						<p><?php echo CFS()->get( 'volunteer_opportunities_text' );	?></p>
-					</div>
-				</div>
 
 			<!--<?php endwhile; // End of the loop. ?>-->
 
