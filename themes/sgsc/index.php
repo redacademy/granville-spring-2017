cefba81ce4600d3b4dac230620af5e1856d2e2a2
Brian Lau <lauwangtatbrian@gmail.com>
2017-06-19 15:28:19 -0700

    add for backup

--- themes/sgsc/index.php
+++ themes/sgsc/index.php
@@ -29,7 +29,25 @@ get_header(); ?>
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<div class="image-position">
+						<?php if ( has_post_thumbnail() ) : ?>
+							<?php the_post_thumbnail( 'large' ); ?>
+						<?php endif; ?>
+					</div>
+					<div class="entry-title">
+						<?php the_title( sprintf( '<h2 ><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+						<?php if ( 'post' === get_post_type() ) : ?>
+						<div class="entry-meta">
+							<?php red_starter_posted_on(); ?>
+						</div><!-- .entry-meta -->
+					</div>
+					<?php endif; ?>
+				</header><!-- .entry-header -->
+			</article><!-- #post-## -->
+
 
 			<?php endwhile; ?>
 
