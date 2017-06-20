5b99f72ace3a46acae3165fc5b7780dcb134367e
Brian Lau <lauwangtatbrian@gmail.com>
2017-06-20 09:19:09 -0700

    remove comment section from single.php

--- themes/sgsc/single.php
+++ themes/sgsc/single.php
@@ -12,16 +12,40 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<?php the_post_navigation(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				 	<div class="donate-btn">
+  				 	<a href="<?php echo esc_url( get_permalink(get_page_by_title( 'donate' )) ) ?>">
+  				 		<i class="fa fa-heart-o" aria-hidden="true">
+  				 			<p>Donate</p>
+  				 		</i>
+  				 		</a>
+  				 </div>
+					<div class="entry-meta">
+						<?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?> / <?php red_starter_posted_by(); ?>
+					</div>
+				</header>
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
+				<div class="entry-content">
+					<?php if ( has_post_thumbnail() ) : ?>
+						<?php the_post_thumbnail( 'large' ); ?>
+					<?php endif; ?>
+
+					<?php the_content(); ?>
+					<?php
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+							'after'  => '</div>',
+						) );
+					?>
+				</div><!-- .entry-content -->
+
+				<footer class="entry-footer">
+					<?php red_starter_entry_footer(); ?>
+				</footer><!-- .entry-footer -->
+			</article><!-- #post-## -->
 
 		<?php endwhile; // End of the loop. ?>
 
