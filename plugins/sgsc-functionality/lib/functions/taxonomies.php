855f5345172afb16c2fa51f759bbd6277c6cce82
Brian Lau <lauwangtatbrian@gmail.com>
2017-06-22 13:15:23 -0700

    Remove archive-gallery taxonomy

--- plugins/sgsc-functionality/lib/functions/taxonomies.php
+++ plugins/sgsc-functionality/lib/functions/taxonomies.php
@@ -5,47 +5,6 @@
  * @link  http://codex.wordpress.org/Function_Reference/register_taxonomy
  */
 
-// Add your custom taxonomies here...
-
-// Register Featured Gallery Taxonomy
-function featured_gallery_taxonomy() {
-
-	$labels = array(
-		'name'                       => 'Featured Galleries',
-		'singular_name'              => 'Featured Gallery',
-		'menu_name'                  => 'Featured Gallery',
-		'all_items'                  => 'All Featured Galleries',
-		'parent_item'                => 'Parent Featured Gallery',
-		'parent_item_colon'          => 'Parent Featured Gallery:',
-		'new_item_name'              => 'New Featured Gallery',
-		'add_new_item'               => 'Add New Featured Gallery',
-		'edit_item'                  => 'Edit Featured Gallery',
-		'update_item'                => 'Update Featured Gallery',
-		'view_item'                  => 'View Featured Gallery',
-		'separate_items_with_commas' => 'Separate Featured Galleries with commas',
-		'add_or_remove_items'        => 'Add or remove Featured Galleries',
-		'choose_from_most_used'      => 'Choose from the most used',
-		'popular_items'              => 'Popular Featured Galleries',
-		'search_items'               => 'Search Featured Galleries',
-		'not_found'                  => 'Not Found',
-		'no_terms'                   => 'No Featured Galleries',
-		'items_list'                 => 'Featured Galleries list',
-		'items_list_navigation'      => 'Featured Galleries list navigation',
-	);
-	$args = array(
-		'labels'                     => $labels,
-		'hierarchical'               => true,
-		'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
-		'show_tagcloud'              => true,
-	);
-	register_taxonomy( 'featured-gallery', array( 'gallery' ), $args );
-
-}
-add_action( 'init', 'featured_gallery_taxonomy', 0 );
-
 // Register Custom Taxonomy
 function Program_Types() {
 
