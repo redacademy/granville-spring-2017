38f39e8357d9dbe5e9bd6e69c9a80f4b02019e22
Kat Solberg <katsolberg@gmail.com>
2017-06-22 15:18:14 -0700

    customize event loop to design

--- themes/sgsc/tribe-events/list/single-event.php
+++ themes/sgsc/tribe-events/list/single-event.php
@@ -11,7 +11,14 @@
  */
 if ( ! defined( 'ABSPATH' ) ) {
 	die( '-1' );
-}
+} ?>
+
+<!-- Event Image -->
+<?php if (has_post_thumbnail()) {
+	echo tribe_event_featured_image( null, 'large' );
+} else { ?>
+	<img src="<?php bloginfo('template_directory'); ?>/assets/images/desktop-logo.svg" alt="<?php the_title(); ?>" />
+<?php } 
 
 // Setup an array of venue details for use later in the template
 $venue_details = tribe_get_venue_details();
@@ -23,20 +30,10 @@ $has_venue_address = ( ! empty( $venue_details['address'] ) ) ? ' location' : ''
 $organizer = tribe_get_organizer();
 
 ?>
-
-<!-- Event Cost -->
-<?php if ( tribe_get_cost() ) : ?>
-	<div class="tribe-events-event-cost">
-		<span><?php echo tribe_get_cost( null, true ); ?></span>
-	</div>
-<?php endif; ?>
-
 <!-- Event Title -->
 <?php do_action( 'tribe_events_before_the_event_title' ) ?>
 <h2 class="tribe-events-list-event-title">
-	<a class="tribe-event-url" href="<?php echo esc_url( tribe_get_event_link() ); ?>" title="<?php the_title_attribute() ?>" rel="bookmark">
 		<?php the_title() ?>
-	</a>
 </h2>
 <?php do_action( 'tribe_events_after_the_event_title' ) ?>
 
@@ -66,18 +63,18 @@ $organizer = tribe_get_organizer();
 </div><!-- .tribe-events-event-meta -->
 <?php do_action( 'tribe_events_after_the_meta' ) ?>
 
-<!-- Event Image -->
-<?php if (has_post_thumbnail()) {
-	echo tribe_event_featured_image( null, 'medium' );
-} else { ?>
-	<img src="<?php bloginfo('template_directory'); ?>/assets/images/desktop-logo.svg" alt="<?php the_title(); ?>" />
-<?php } ?>
+<!-- Event Cost -->
+<?php if ( tribe_get_cost() ) : ?>
+	<div class="tribe-events-event-cost">
+		<span><?php echo tribe_get_cost( null, true ); ?></span>
+	</div>
+<?php endif; ?>
 
 <!-- Event Content -->
 <?php do_action( 'tribe_events_before_the_content' ); ?>
 <div class="tribe-events-list-event-description tribe-events-content description entry-summary">
-	<?php echo tribe_events_get_the_excerpt( null, wp_kses_allowed_html( 'post' ) ); ?>
-	<a href="<?php echo esc_url( tribe_get_event_link() ); ?>" class="tribe-events-read-more" rel="bookmark"><?php esc_html_e( 'Find out more', 'the-events-calendar' ) ?> &raquo;</a>
+	<?php echo the_content( null, wp_kses_allowed_html( 'post' ) ); ?>
+	
 </div><!-- .tribe-events-list-event-description -->
 <?php
 do_action( 'tribe_events_after_the_content' );
