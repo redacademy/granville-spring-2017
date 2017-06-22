3ca00bd896bb12ac025ecf0035265c47c41f6a84
Kat Solberg <katsolberg@gmail.com>
2017-06-22 09:37:07 -0700

    remove event custom post type

--- plugins/sgsc-functionality/lib/functions/post-types.php
+++ plugins/sgsc-functionality/lib/functions/post-types.php
@@ -62,62 +62,6 @@ function gallery_post_type() {
 add_action( 'init', 'gallery_post_type', 0 );
 
 // Register Custom Post Type
-function events_post_type() {
-
-	$labels = array(
-		'name'                  => 'Events',
-		'singular_name'         => 'Event',
-		'menu_name'             => 'Events',
-		'name_admin_bar'        => 'Event',
-		'archives'              => 'Event Archives',
-		'attributes'            => 'Event Attributes',
-		'parent_item_colon'     => 'Parent Event:',
-		'all_items'             => 'All Events',
-		'add_new_item'          => 'Add New Event',
-		'add_new'               => 'Add New',
-		'new_item'              => 'New Event',
-		'edit_item'             => 'Edit Event',
-		'update_item'           => 'Update Event',
-		'view_item'             => 'View Event',
-		'view_items'            => 'View Events',
-		'search_items'          => 'Search Event',
-		'not_found'             => 'Not found',
-		'not_found_in_trash'    => 'Not found in Trash',
-		'featured_image'        => 'Featured Image',
-		'set_featured_image'    => 'Set featured image',
-		'remove_featured_image' => 'Remove featured image',
-		'use_featured_image'    => 'Use as featured image',
-		'insert_into_item'      => 'Insert into event',
-		'uploaded_to_this_item' => 'Uploaded to this event',
-		'items_list'            => 'Events list',
-		'items_list_navigation' => 'Events list navigation',
-		'filter_items_list'     => 'Filter events list',
-	);
-	$args = array(
-		'label'                 => 'Event',
-		'description'           => 'A calendar',
-		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor', 'thumbnail', 'custom-fields', ),
-		'hierarchical'          => false,
-		'public'                => true,
-		'show_ui'               => true,
-		'show_in_menu'          => true,
-		'menu_position'         => 5,
-		'menu_icon'             => 'dashicons-calendar',
-		'show_in_admin_bar'     => true,
-		'show_in_nav_menus'     => true,
-		'can_export'            => true,
-		'has_archive'           => true,		
-		'exclude_from_search'   => false,
-		'publicly_queryable'    => true,
-		'capability_type'       => 'post',
-	);
-	register_post_type( 'events', $args );
-
-}
-add_action( 'init', 'events_post_type', 0 );
-
-// Register Custom Post Type
 function custom_program() {
 
 	$labels = array(
