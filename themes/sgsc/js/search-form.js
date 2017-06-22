ed0ac87c976c17c3bca2d18a84ddf636e27f7155
heyliddle <eric.liddle@gmail.com>
2017-06-22 09:16:48 -0700

    adjusted the search toggle

--- themes/sgsc/js/search-form.js
+++ themes/sgsc/js/search-form.js
@@ -7,25 +7,25 @@
         $('.search-field').focus();
         return false;
     });
-    $(document).click( function(event){
-      if( !$('.search-field').is(event.target)){
-      $('.search-field').hide('slow');
-      $('.icon-search').show();
-      }
-    });
-  });
-
-})(jQuery);
-
-
 //     $(document).click( function(event){
 //       if( !$('.search-field').is(event.target)){
 //       $('.search-field').hide('slow');
-//       setTimeout(function(){
-//         $('.icon-search').show();
-//       }, 487)
+//       $('.icon-search').show();
 //       }
 //     });
 //   });
 
 // })(jQuery);
+
+
+    $(document).click( function(event){
+      if( !$('.search-field').is(event.target)){
+      $('.search-field').hide('slow');
+      setTimeout(function(){
+        $('.icon-search').show();
+      }, 487)
+      }
+    });
+  });
+
+})(jQuery);
