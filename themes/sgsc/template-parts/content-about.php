9a90b75099c38b1db9bb9cd0553beb2e49cd3f52
heyliddle <eric.liddle@gmail.com>
2017-06-23 14:51:06 -0700

    styled about-us page

--- themes/sgsc/template-parts/content-about.php
+++ themes/sgsc/template-parts/content-about.php
@@ -8,7 +8,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-content">
+	<div class="about-content">
 		<?php the_content(); ?>
 		<!---->
 	</div><!-- .entry-content -->
