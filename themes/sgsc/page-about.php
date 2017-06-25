14c754803224428051fb0d2ebaec1191eff9573e
heyliddle <eric.liddle@gmail.com>
2017-06-24 21:05:47 -0700

    styling about page

--- themes/sgsc/page-about.php
+++ themes/sgsc/page-about.php
@@ -8,22 +8,32 @@
 
 get_header(); ?>
 
+	<section class="header">
+		<div class="entry-header">
+			<!--<div class="about-hero-wrapper"> -->
+			<?php if ( has_post_thumbnail() ) {
+        		the_post_thumbnail();
+        	} ?> 
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			<!--</div>-->
+			<div class="donate-btn">
+				<a href="<?php echo esc_url( get_permalink(get_page_by_path( 'donate' )) ) ?>">
+				<i class="fa fa-heart-o" aria-hidden="true"></i>
+				</a>
+			</div>
+		</div>
+	</section><!-- .entry-header -->
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main container" role="main">
-      <?php if ( has_post_thumbnail() ) {
-        $header_image = get_the_post_thumbnail_url();
-        } ?>
+
         <!--the_content();?>-->
 
-		<div class="about-header" style="background-image: url(<?php echo $header_image ?>); background-size: cover; height: 353px; background-position: center;">
+		<?php while ( have_posts() ) : the_post(); ?>
 
-		</div>
+			<?php get_template_part( 'template-parts/content', 'about' ); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'about' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
+		<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
--- themes/sgsc/template-parts/content-about.php
+++ themes/sgsc/template-parts/content-about.php
@@ -8,7 +8,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="about-content">
+	<div class="entry-content">
 		<?php the_content(); ?>
 		<!---->
 	</div><!-- .entry-content -->
