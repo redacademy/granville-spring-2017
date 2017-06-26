0c330d525343ea253b893d4a6e41ed5cf99240e3
heyliddle <eric.liddle@gmail.com>
2017-06-26 15:43:36 -0700

    fixed issues with styling on partners and members

---
+++ themes/sgsc/page-about-our-partners.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Template Name: About-Our-Partners
+ *
+ *
+ * @package SGSC_Theme
+ */
+
+get_header(); ?>
+
+	<section class="header">
+		<div class="entry-header">
+			<!--<div class="about-hero-wrapper"> -->
+			<?php if ( has_post_thumbnail() ) {
+        		the_post_thumbnail();
+        	} ?> 
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			<!--</div>-->
+			<div class="donate-btn">
+				<a href="<?php echo esc_url( get_permalink(get_page_by_path( 'donate' )) ) ?>">
+				<i class="fa fa-heart-o" aria-hidden="true"></i>
+				</a>
+			</div>
+		</div>
+	</section><!-- .entry-header -->
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main container" role="main">
+
+        <!--the_content();?>-->
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'about' ); ?>
+
+		<?php endwhile; // End of the loop. ?>
+			<ul class="team-members">
+				<?php
+					$fields = CFS()->get( 'our_partners' );
+					foreach ( $fields as $field ) : ?>
+					<li class="member">
+						<img class="member-picture" src="<?php echo $field['partner_picture'] ?>" alt=""/>
+						<h1 class="member-name member-container"> <?php echo $field['partner_name']; ?></h1>
+						<p class="member-position member-container"> <?php echo $field['partner_position']; ?></p>
+						<p class="member-email member-container"> <?php echo $field['partner_email']; ?></p>
+						<p class="member-bio member-container"> <?php echo  $field['partner_bio'] ?></p>
+					</li>
+				<?php endforeach ?>
+			</ul>
+			<div class="about-back-btn">
+			<a href="<?php echo esc_url( get_permalink (get_page_by_path('about'))) ?>">
+				<p>Back</p>
+			</a>
+		</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
