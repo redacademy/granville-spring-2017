f842218b28b14a8b85858c4ae495a16dae501d02
heyliddle <eric.liddle@gmail.com>
2017-06-27 15:21:06 -0700

    fixed hidden menu on resize from mobile to desktop

--- themes/sgsc/js/navigation.js
+++ themes/sgsc/js/navigation.js
@@ -1,25 +1,36 @@
-( function($) {
-  $(document).ready(function() {
+(function ($) {
+  $(document).ready(function () {
 
     var $hamburger = $('.menu-toggle'),
-        $content = $('.site-content'),
-        $mobileMenu = $('.nav-menu'),
-        $overlay = $('.overlay'),
-        $backButton = $('#back-button');
+      $content = $('.site-content'),
+      $mobileMenu = $('.nav-menu'),
+      $overlay = $('.overlay'),
+      $backButton = $('#back-button');
 
-    $hamburger.click(function (){
-      $content.css({'height': '0', 'overflow': 'hidden'});
+    $hamburger.click(function () {
+      $content.css({ 'height': '0', 'overflow': 'hidden' });
       $overlay.addClass('overlay-open').slideDown('1000');
-      $mobileMenu.toggleClass('menu-visible');      
+      $mobileMenu.toggleClass('menu-visible');
       $backButton.css('display', 'initial');
     });
-    $backButton.click(function(){
-      $content.css({'height': 'auto', 'overflow': 'scroll'});      
+    $backButton.click(function () {
+      $content.css({ 'height': 'auto', 'overflow': 'scroll' });
       $overlay.slideUp('slow');
       $mobileMenu.toggleClass('menu-visible');
       $backButton.css('display', 'none');
       $overlay.removeClass('overlay-open');
     });
+    (function ($) {
+      var $window = $(window),
+          $overlay = $('.overlay');
+
+      $window.resize(function resize() {
+        if ($window.width() > 480) {
+        return $overlay.addClass('desktopWidth');
+        }
+      $overlay.removeClass('desktopWidth');
+      }).trigger('resize');
+    })(jQuery);
   });
 
 })(jQuery);
